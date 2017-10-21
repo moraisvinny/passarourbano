9a18d6a9a57102aa4ce0d6419562a1e081f5d239
Vinicius Morais <vinicius.reis.morais@gmail.com>
2017-10-21 18:07:33 -0200

    pesquisa por restaurantes

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -12,9 +12,15 @@ export class OfertasService {
     public getOfertas(): Promise<Oferta[]> {
         
         return this.http.get("http://localhost:3000/ofertas?destaque=true")
-        .toPromise()
-        .then((resposta: any) => resposta.json())
+            .toPromise()
+            .then((resposta: any) => resposta.json())
         
+    }
+
+    public getOfertasPorCategiruas(categoria: string): Promise<Oferta[]> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+            .toPromise()
+            .then((resposta: any) => resposta.json());
     }
 
     
