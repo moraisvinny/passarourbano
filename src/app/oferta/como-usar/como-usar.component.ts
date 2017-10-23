30f7614cc6ea21640c22de3565a74516b65820b5
Vinicius Morais <vinicius.reis.morais@gmail.com>
2017-10-23 18:56:43 -0200

    componentes como-usar e onde-fica

---
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { OfertasService } from '../../ofertas.service'
+
+@Component({
+  selector: 'app-como-usar',
+  templateUrl: './como-usar.component.html',
+  styleUrls: ['./como-usar.component.css'],
+  providers: [OfertasService]
+})
+export class ComoUsarComponent implements OnInit {
+
+  public comoUsar: string = ''
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) { }
+
+  ngOnInit() {
+    let id = this.route.parent.snapshot.params['id']
+    this.ofertasService.getComoUsarOfertaPorId(id)
+      .then((descricao: string) => {
+        this.comoUsar = descricao
+      })
+  }
+
+}
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Oferta } from './shared/oferta.model'
 import {Injectable} from '@angular/core'
 import { Http } from '@angular/http'
+import { API_URL } from './app.api'
 
 import 'rxjs/add/operator/toPromise'
 
@@ -11,23 +12,27 @@ export class OfertasService {
 
     public getOfertas(): Promise<Oferta[]> {
         
-        return this.http.get("http://localhost:3000/ofertas?destaque=true")
+        return this.http.get(`${API_URL}/ofertas?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
         
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${API_URL}/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta.json());
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+        return this.http.get(`${API_URL}/ofertas?id=${id}`)
             .toPromise().then((resposta) => {return resposta.json().shift()})
     }
 
+    public getComoUsarOfertaPorId(id: number): Promise<String> {
+        return this.http.get(`${API_URL}/como-usar?id=${id}`)
+            .toPromise().then((resposta) => {return resposta.json()[0].descricao})
+    }
     
 
 }
