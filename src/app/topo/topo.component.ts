a1628fa22948dcb9d6f4ec7130460b2a316c797d
Vinicius Morais <vinicius.reis.morais@gmail.com>
2017-10-31 18:53:55 -0200

    fim observables e inicio pipes

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -5,35 +5,50 @@ import { Subject } from 'rxjs/Subject'
 import { OfertasService } from '../ofertas.service'
 import { Oferta } from '../shared/oferta.model'
 
-import 'rxjs/add/operator/switchMap'
+import '../util/rxjs-extensions'
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
   private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
 
-    this.ofertas = this.subjectPesquisa.switchMap((termo: string) => {
-      console.log("Executei o subject")
-      return this.ofertasService.pesquisarOfertas(termo)
-    })
+    this.ofertas = this.subjectPesquisa
+      .debounceTime(1000)
+      .distinctUntilChanged()
+      .switchMap((termo: string) => {
+        console.log("Executei o subject")
+        if(termo.trim() === '') {
+          return Observable.of<Oferta[]>([])
+        }
+        return this.ofertasService.pesquisarOfertas(termo)
+      })
+      .catch((erro: any) =>{
+        console.log(erro)
+        return Observable.of<Oferta[]>([])
+      })
 
-    this.subjectPesquisa.subscribe({next: (ofertas: string) => console.log(ofertas)})
-    this.ofertas.subscribe({next: (ofertas: Oferta[]) => console.log(ofertas)})
-    
+    //this.subjectPesquisa.subscribe({next: (ofertas: string) => console.log(ofertas)})
+    this.ofertas.subscribe({ next: (ofertas: Oferta[]) => {
+      console.log(ofertas) 
+      this.ofertas2 = ofertas
+    }})
+
   }
   pesquisar(valor: string): void {
+
+      this.subjectPesquisa.next(valor)
     
-    this.subjectPesquisa.next(valor)
   }
 
 }
