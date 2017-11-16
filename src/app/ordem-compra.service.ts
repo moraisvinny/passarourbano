3719f4b001664d9ed734cebb4f20f425bf7f1243
Vinicius Morais <vinicius.reis.morais@gmail.com>
2017-11-16 00:20:32 -0200

    validacoes de formulario

---
+++ src/app/ordem-compra.service.ts
@@ -0,0 +1,8 @@
+import { Pedido } from './shared/pedido.model'
+
+export class OrdemCompraService {
+
+    public efetivarCompra(pedido: Pedido): void {
+        console.log("Efetivar Compra -> ", pedido)
+    }
+}
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdemCompraService } from '../ordem-compra.service'
+import { Pedido } from '../shared/pedido.model'
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
 
@@ -11,29 +14,64 @@ export class OrdemCompraComponent implements OnInit {
   public numero: string = ''
   public complemento: string = ''
   public formaPagamento: string = ''
-  constructor() { }
+
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean  = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean  = true
+
+  public formEstado: string = 'disabled'
+
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
   }
 
   public setEndereco(endereco: string) : void {
     this.endereco = endereco
-    console.log(this.endereco)
+    this.enderecoEstadoPrimitivo = false
+
+    this.enderecoValido = this.endereco.length > 3
+    this.habilitaForm()
+
   }
 
   public setNumero(numero: string) : void {
     this.numero = numero
-    console.log(this.numero)
-  }
+    this.numeroEstadoPrimitivo = false
+    this.numeroValido = this.numero != ''
+    this.habilitaForm()
+  } 
 
   public setComplemento(compelemento: string) : void {
     this.complemento = compelemento
-    console.log(this.complemento)
+    this.complementoEstadoPrimitivo = false
+    this.complementoValido = this.complemento != ''
+    this.habilitaForm()
   }
 
   public setFormaPagamento(formaPagamento: string) : void {
     this.formaPagamento = formaPagamento
-    console.log(this.formaPagamento)
+    this.formaPagamentoEstadoPrimitivo = false
+    this.formaPagamentoValido  = formaPagamento !== ''
+    this.habilitaForm()
   }
 
+  public habilitaForm(): void {
+    if(this.enderecoValido && this.numeroValido && this.formaPagamentoValido) {
+      this.formEstado = ''
+    } else {
+      this.formEstado = 'disabled'
+    }
+  }
+
+  public confirmarCompra() {
+    var pedido: Pedido = new Pedido(this.endereco, this.numero,this.complemento, this.formaPagamento)
+    this.ordemCompraService.efetivarCompra(pedido)
+  }
 }
